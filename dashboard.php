e1b1426403927bc01c5377213b29c26fdaa8fff8
Maximo Sojo <maxsojo13@gmail.com>
2019-11-04 22:14:02 -0400

    refactor(Prueba)

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@
     <div class="right_col" role="main"> <!-- page content -->
         <div class="">
             <div class="page-title">
-                <!-- <div class="row top_tiles">
+                <div class="row top_tiles">
                     <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
                         <div class="tile-stats">
                           <div class="icon"><i class="fa fa-ticket"></i></div>
@@ -40,7 +40,7 @@
                           <h3>Usuarios</h3>
                         </div>
                     </div>
-                </div> -->
+                </div>
                 <!-- content -->
                 <br><br>
                 <div class="row">
